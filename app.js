14236fff9ce6652e859495ff6e125975ba20eea4
Omar Zidan <ziom18fy@student.ju.se>
2019-09-05 18:19:17 +0200

    added few khara

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ app.engine("hbs", expressHandlebars({
 
 app.get('/', function(request, response){
   const model = {
-    humans: dummyData.humans
+    humans: dummyData.humans,
+    pets : dummyData.pets
   }
   response.render("show-all-humans.hbs", model)
+})
+
+app.get('/home', function(request, response){
+  response.render("home.hbs")
+})
+
+app.get('/about', function(request, response){
+  response.render("about.hbs")
+})
+app.get('/contact', function(request, response){
+  response.render("contact.hbs")
 })
 
 app.listen(8080)
